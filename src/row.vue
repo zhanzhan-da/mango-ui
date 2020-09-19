a7f3a5d6a947f9e3dcafdc349ddd16f25dab031e
aaccbbmll <m13715332956@163.com>
2020-09-19 16:18:04 +0800

    重构 col row

--- src/row.vue
+++ src/row.vue
@@ -1,8 +1,5 @@
 <template>
-  <div
-    class="row"
-    :style="{ marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px' }"
-  >
+  <div class="row" :style="rowStyle">
     <slot></slot>
   </div>
 </template>
@@ -15,7 +12,15 @@ export default {
       type: [Number, String]
     }
   },
-  created() {},
+  computed: {
+    rowStyle() {
+      let { gutter } = this;
+      return {
+        marginLeft: -gutter / 2 + 'px',
+        marginRight: -gutter / 2 + 'px'
+      };
+    }
+  },
   mounted() {
     this.$children.forEach((vm) => {
       vm.gutter = this.gutter;
