c8fc85d9f16f990dfa74a70f762b359146bd7c5b
aaccbbmll <m13715332956@163.com>
2020-09-27 15:25:21 +0800

    aaa

--- test/tabs-item.test.js
+++ test/tabs-item.test.js
@@ -1,45 +1,45 @@
-// const expect = chai.expect;
-// import Vue from 'vue';
-// import Tabs from '../src/tabs';
-// import TabsHead from '../src/tabs-head';
-// import TabsBody from '../src/tabs-body';
-// import TabsItem from '../src/tabs-item';
-// import TabsPane from '../src/tabs-pane';
+const expect = chai.expect;
+import Vue from 'vue';
+import Tabs from '../src/tabs';
+import TabsHead from '../src/tabs-head';
+import TabsBody from '../src/tabs-body';
+import TabsItem from '../src/tabs-item';
+import TabsPane from '../src/tabs-pane';
 
-// Vue.component('m-tabs', Tabs);
-// Vue.component('m-tabs-head', TabsHead);
-// Vue.component('m-tabs-body', TabsBody);
-// Vue.component('m-tabs-item', TabsItem);
-// Vue.component('m-tabs-pane', TabsPane);
+Vue.component('m-tabs', Tabs);
+Vue.component('m-tabs-head', TabsHead);
+Vue.component('m-tabs-body', TabsBody);
+Vue.component('m-tabs-item', TabsItem);
+Vue.component('m-tabs-pane', TabsPane);
 
-// Vue.config.productionTip = false;
-// Vue.config.devtools = false;
+Vue.config.productionTip = false;
+Vue.config.devtools = false;
 
-// describe('TabsItem', () => {
-//   it('存在.', () => {
-//     expect(TabsItem).to.exist;
-//   });
+describe('TabsItem', () => {
+  it('存在.', () => {
+    expect(TabsItem).to.exist;
+  });
 
-//   it('接受 name 属性', () => {
-//     const Constructor = Vue.extend(TabsItem);
-//     const vm = new Constructor({
-//       propsData: {
-//         name: 'xxx'
-//       }
-//     }).$mount();
-//     expect(vm.$el.getAttribute('data-name')).to.eq('xxx');
-//   });
-//   it('接受 disabled 属性', () => {
-//     const Constructor = Vue.extend(TabsItem);
-//     const vm = new Constructor({
-//       propsData: {
-//         disabled: true
-//       }
-//     }).$mount();
-//     expect(vm.$el.classList.contains('disabled')).to.be.true;
-//     const callback = sinon.fake();
-//     vm.$on('click', callback);
-//     vm.$el.click();
-//     expect(callback).to.have.not.been.called;
-//   });
-// });
+  it('接受 name 属性', () => {
+    const Constructor = Vue.extend(TabsItem);
+    const vm = new Constructor({
+      propsData: {
+        name: 'xxx'
+      }
+    }).$mount();
+    expect(vm.$el.getAttribute('data-name')).to.eq('xxx');
+  });
+  it('接受 disabled 属性', () => {
+    const Constructor = Vue.extend(TabsItem);
+    const vm = new Constructor({
+      propsData: {
+        disabled: true
+      }
+    }).$mount();
+    expect(vm.$el.classList.contains('disabled')).to.be.true;
+    const callback = sinon.fake();
+    vm.$on('click', callback);
+    vm.$el.click();
+    expect(callback).to.have.not.been.called;
+  });
+});
