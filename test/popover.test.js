6e14c50fc41ecc01131a80a81cf692f1ace1b7b0
aaccbbmll <m13715332956@163.com>
2020-09-27 15:14:29 +0800

    修改测试用例

--- test/popover.test.js
+++ test/popover.test.js
@@ -38,12 +38,12 @@ describe('Popover', () => {
     const div = document.createElement('div')
     document.body.appendChild(div)
     div.innerHTML = `
-    <g-popover trigger="hover" ref="a">
+    <m-popover trigger="hover" ref="a">
       <template slot="content">
       弹出内容
       </template>
       <button>点我</button>
-    </g-popover>
+    </m-popover>
     `
     const vm = new Vue({
       el: div
