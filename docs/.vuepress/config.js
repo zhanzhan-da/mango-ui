fe2817c969d741493dbbea2d71706c85874ae929
aaccbbmll <m13715332956@163.com>
2020-10-01 13:52:41 +0800

    vuepress基础搭建

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,37 @@
+module.exports = {
+    title: '芒果UI官网',
+    description: '一个好用的UI组件库',
+    themeConfig: {
+        displayAllHeaders: true,
+        nav: [
+          { text: '主页', link: '/' },
+          { text: '文档', link: '/basic/install' },
+          { text: 'GitHub', link: 'https://www.baidu.com' },
+        ],
+        sidebar: [
+          {
+            title:'入门',
+            collapsable: false, // 可选的, 默认值是 true,
+            children: [
+              '/basic/install',
+              '/basic/usage',
+            ]
+          },
+          {
+            title: '组件',
+            collapsable: false, // 可选的, 默认值是 true,
+            sidebarDepth: 0,    // 可选的, 默认值是 1
+            children: [
+              '/components/button',
+              '/components/input',
+              '/components/grid',
+              '/components/layout',
+              '/components/tabs',
+              '/components/popover',
+              '/components/toast',
+              '/components/collapse',
+            ],
+          }
+        ],
+      }
+  }
